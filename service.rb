2c599e44c00073c312094553e98bf0e5c6c10584
versaillo <jalvin92@gmail.com>
2016-04-27 02:13:53 +0800

    Update service.rb

--- service.rb
+++ service.rb
@@ -8,9 +8,25 @@ post '/payload' do
         if var["changes"]["title"]
             puts "issue edited. Title changed from: " + var["changes"]["title"]["from"]
             var["action"] = "updated"
+            httppost(var)
         elsif var["changes"]["body"]
             puts "issue edited. Body changed from: " + var["changes"]["body"]["from"]
             var["action"] = "updated"
+            httppost(var)
         end
     end
+end
+
+def httppost(var)
+    host = ''
+    port = ''
+    #user = ''
+    #pass = ''
+    post_ws = "" #workspace
+    req = Net::HTTP::Post.new(post_ws, initheader = {'Content-Type' =>'application/json'})
+    #req.basic_auth user, pass  
+    req.body = var.to_json
+    http = Net::HTTP.new(host, port)
+    response = http.start {|http1| http1.request(req) }
+    puts "Response #{response.code} #{response.message}: #{response.body}"
 end
